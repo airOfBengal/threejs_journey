7c3992a24cafb595d88519b170db2db62a201697
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-12-11 23:11:36 +0600

    add object transformation

--- 03/exercise/script.js
+++ 03/exercise/script.js
@@ -4,12 +4,17 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 // Object
 const myGeometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
 const mesh = new THREE.Mesh(myGeometry, material)
+// mesh.position.set(0.7, -0.6, 1)
+// mesh.scale.set(2, 0.25, 0.5)
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
 scene.add(mesh)
 
 // Sizes
@@ -21,6 +26,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
